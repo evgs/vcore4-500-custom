5b37c56457a92b38f8cd6c8a0dba6d977b70cd89
Eugeniy Stahov <eugene.stahov@gmail.com>
2024-12-20 16:37:06 +0500

    do make config before firmware build to ensure new default flags will be applied to .config file

--- home/mcu-update/buildall.sh
+++ home/mcu-update/buildall.sh
@@ -8,14 +8,17 @@ mkdir -p $MUPD/manta
 mkdir -p $MUPD/ebb
 
 cp $MUPD/klipper-config-main-h723 .config
+make config
 make || die Manta build failed
 cp out/klipper.bin $MUPD/manta
 
 cp $MUPD/klipper-config-ebb-g0b1 .config
+make config
 make || die EBB build failed
 cp out/klipper.bin $MUPD/ebb
 
 cp $MUPD/klipper-config-linuxhost .config
+make config
 make || die MCU Host build failed
 
 sudo systemctl stop klipper-mcu.service
