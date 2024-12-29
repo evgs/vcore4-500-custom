fafc5de70883feed74d9b414c8987a7cf4bb1a0c
Eugene Stahov <eugene.stahov@gmail.com>
2024-12-29 23:27:13 +0500

    Update buildall.sh - olddefconfig

--- home/mcu-update/buildall.sh
+++ home/mcu-update/buildall.sh
@@ -8,17 +8,17 @@ mkdir -p $MUPD/manta
 mkdir -p $MUPD/ebb
 
 cp $MUPD/klipper-config-main-h723 .config
-make config
+make olddefconfig
 make || die Manta build failed
 cp out/klipper.bin $MUPD/manta
 
 cp $MUPD/klipper-config-ebb-g0b1 .config
-make config
+make olddefconfig
 make || die EBB build failed
 cp out/klipper.bin $MUPD/ebb
 
 cp $MUPD/klipper-config-linuxhost .config
-make config
+make olddefconfig
 make || die MCU Host build failed
 
 sudo systemctl stop klipper-mcu.service
